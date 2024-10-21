e95fddb5bc9637b5387b70f04e1a2948de82c851
Abhishek Yadav <148934908+Abhidev-aiml@users.noreply.github.com>
2024-10-22 02:12:06 +0530

    fe and be done

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,11 @@
+import Homepage from "./homepage/page";
 
 
 export default function Home() {
   return (
 
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        
+      <Homepage />
 </main>
   );
 }
